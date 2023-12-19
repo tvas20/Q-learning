9d8681e66987dbd6f95e5bbc58b70eb3591af250
Tiago Victor de Amorim Silva <tvas@cin.ufpe.br>
2023-12-19 14:46:22 -0300

    feat: add epsilon greedy and train the q-table

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import random
 import connection as cn
+import numpy as np
 
 def txtFileToMatrix():
     matrix = []
@@ -17,10 +18,17 @@ def matrixToTxtFile(matrix):
     for line in matrix:
         f.writelines(str(line[0])+' '+str(line[1])+' '+str(line[2])+'\n')
 
-def q_update(previous_state, action, state, rw, q_matrix, alpha, gamma):
+def q_update(previous_state, action, state, rw, q_matrix, alpha, gamma): # q-learning equation
     estimate_q = rw + gamma * (max(q_matrix[state]))
     q_value = q_matrix[previous_state][action] + alpha*(estimate_q - q_matrix[previous_state][action])
     return q_value
+
+def escolha_acao(epislon, state): #epsilon greedy
+    if np.random.uniform(0, 1) < epislon:
+        acao = random.randint(0, 2)
+    else:
+        acao = np.argmax(state)
+    return acao
 
 def main():
     # Faz a conexão com o jogo
@@ -30,25 +38,28 @@ def main():
 
     # Estado inicial
     estado = 84 # o boneco está na plataforma 21 olhado para o norte
-
-    alpha = 0.2 # Taxa de aprendizado
+    epislon = 0.1 # epsilon (taxa de aleatoriedade da ação)
+    alpha = 0.1 # Taxa de aprendizado
     gamma = 0.9 # Fator de desconto
 
     recompensa = -1
-    
-    while recompensa < 0:
-        # Estado antes de aplicar a ação
-        estado_anterior = estado
-        
-        selected_action = random.randint(0,2)
-        estado, recompensa = cn.get_state_reward(s, actions[selected_action])
+    for i in range(100):
+        recompensa = -1
+        while recompensa < 0:
+            # Estado antes de aplicar a ação
+            estado_anterior = estado
 
-        # transforma o estado de binário para int
-        estado = int(estado, 2)
+            #escolha da ação a ser tomada
+            selected_action = escolha_acao(epislon, q_matrix[estado_anterior])
+            estado, recompensa = cn.get_state_reward(s, actions[selected_action])
 
-        q_matrix[estado_anterior][selected_action] = q_update(estado_anterior, selected_action, estado, recompensa, q_matrix, alpha, gamma)
-        print(estado,recompensa)
-    print('CHEGOU AO OBJETIVO')
+            # transforma o estado de binário para int
+            estado = int(estado, 2)
+
+            #atualiza a recompensa na q-table
+            q_matrix[estado_anterior][selected_action] = q_update(estado_anterior, selected_action, estado, recompensa, q_matrix, alpha, gamma)
+            print(estado,recompensa)
+        print('CHEGOU AO OBJETIVO')
 
 
     
